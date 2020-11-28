cb92a967608fa7d7a63fe5b5d7f7c1ca46c3be77
pragmaticreviews <info@pragmaticreviews.com>
2020-11-28 12:27:37 -0300

    Basic REST API.

--- pages/api/jobs/[jobId].js
+++ pages/api/jobs/[jobId].js
@@ -1,12 +1,36 @@
 import { jobs } from '../../../data'
 
-export default async ({ query: { jobId } }, res) => {
+export default async (req, res) => {
+  const { query: { jobId }, body: { title, company, location }, method } = req;
   const filtered = jobs.filter((job) => job.id === parseInt(jobId))
 
-  // Job with id exists
-  if (filtered.length > 0) {
-    res.status(200).json(filtered[0])
-  } else {
-    res.status(404).json({ message: `Job with id: ${jobId} not found.` })
+  switch (method) {
+    case 'GET':
+      // Get data
+      // Job with id exists
+      if (filtered.length > 0) {
+           res.status(200).json(filtered[0])
+      } else {
+           res.status(404).json({ message: `Job with id: ${jobId} not found.` })
+      }
+      break
+    case 'PUT':
+      // Update data (TODO)
+      // Return Updated resource
+      res.status(200).json({
+          id: jobId, 
+          title: title, 
+          company: company, 
+          location: location
+        });
+      break
+    case 'DELETE':
+        // Delete data (TODO)
+        // Return deleted resource
+        res.status(200).json(filtered[0]);
+        break      
+    default:
+      res.setHeader('Allow', ['GET', 'PUT', 'DELETE'])
+      res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
--- pages/api/jobs/index.js
+++ pages/api/jobs/index.js
@@ -1,5 +1,30 @@
 import { jobs } from '../../../data'
 
-export default async (_, res) => {
-  res.status(200).json(jobs)
+export default (req, res) => {
+  const { body: { title, company, location }, method } = req;
+
+  switch(method) {
+    case 'GET':
+      // Get List of Jobs
+      res.status(200).json(jobs)        
+      break
+    case 'POST':
+      // Add new data (TODO)
+      // Return new resource  
+      res.status(200).json({
+        title: title, 
+        company: company, 
+        location: location
+      });
+      break
+    case 'OPTIONS':
+      res.setHeader('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
+      res.setHeader('Access-Control-Allow-Headers', ['Content-Type'])
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.status(200).end()
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }  
 }
