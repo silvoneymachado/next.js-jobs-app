38e977e9f0583b9ae6ca85973b6bc8b8e832a461
pragmaticreviews <info@pragmaticreviews.com>
2021-02-12 21:42:40 -0300

    Slack Sign In with NextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,30 +3,10 @@ import Providers from 'next-auth/providers'
 
 const options = {
   providers: [
-    // Passwordless / email sign in
-    Providers.Email({
-        server: {
-          host: process.env.EMAIL_SERVER_HOST,
-          port: process.env.EMAIL_SERVER_PORT,
-          auth: {
-            user: process.env.EMAIL_SERVER_USER,
-            pass: process.env.EMAIL_SERVER_PASSWORD
-          }
-        },
-        from: process.env.EMAIL_FROM
-    }),
-    Providers.Spotify({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      profile(profile) {
-        return {
-          id: profile.id,
-          name: profile.display_name,
-          email: profile.email,
-          image: profile.images?.[0]?.url
-        }
-      },
-    }),
+    Providers.Slack({
+      clientId: process.env.SLACK_CLIENT_ID,
+      clientSecret: process.env.SLACK_CLIENT_SECRET
+    })
   ],
   // Optional SQL or MongoDB database to persist users
   database: process.env.DATABASE_URL
