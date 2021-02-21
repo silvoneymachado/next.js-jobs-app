110ea31f180bd24760b909ae2368e2bf91a53fc1
pragmaticreviews <info@pragmaticreviews.com>
2021-02-21 18:26:59 -0300

    Setting up custom Credentials provider with JWT

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,13 +3,39 @@ import Providers from 'next-auth/providers'
 
 const options = {
   providers: [
-    Providers.GitLab({
-      clientId: process.env.GITLAB_CLIENT_ID,
-      clientSecret: process.env.GITLAB_CLIENT_SECRET
-    })
+    Providers.Credentials({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      // The credentials property is used to generate a suitable form on the sign in page.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: {  label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null or false then the credentials will be rejected
+          return null
+          // You can also Reject this callback with an Error or with a URL:
+          // throw new Error('error message') // Redirect to error page
+          // throw '/path/to/redirect'        // Redirect to a URL
+        }
+      }
+    })    
   ],
-  // Optional SQL or MongoDB database to persist users
-  database: process.env.DATABASE_URL
+  session: { 
+    jwt: true,
+  },
+  jwt: {
+    // A secret to use for key generation - you should set this explicitly
+    // Defaults to NextAuth.js secret if not explicitly specified.
+    secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw',    
+  }  
 }
 
 export default (req, res) => NextAuth(req, res, options);
